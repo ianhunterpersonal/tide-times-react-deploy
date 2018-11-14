6f56e328b06a55ac25947be4beeda458d944651c
Ian Hunter <ian.hunter@ad.mirada.local>
2018-11-14 14:15:44 +0000

    Another update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dc2011bd498e44271f95feb8853056fb.js"
+  "/precache-manifest.f92c4fbc61ebb2c641af2de035f7e064.js"
 );
 
 workbox.clientsClaim();
